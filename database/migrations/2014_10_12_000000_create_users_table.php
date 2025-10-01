c177bba148e2e529258c4aa2ad47251fd44ca33a
Bramahimsa <bramahimsa123@gmail.com>
2025-10-02 00:10:13 +0700

    menyusul dari pert 3nih

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,18 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nama');
+            $table->string('nim');
+            $table ->foreignkey('kelas_id') -> contrained();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 
 
 Route::get('/profile/{nama?}/{kelas?}/{npm?}', [ProfileController::class, 'profile']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/create', [UserController::class, 'create']) -> name('user.create');
+Route::get('/user', [UserController::class, 'store']) -> name('user.store');
+
 
 Route::get('/', function () {
     return view('welcome');
